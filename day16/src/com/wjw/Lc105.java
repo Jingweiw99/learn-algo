aa8ea862d2ee44d93fa152aaec04206d97fa829d
wjw <Wangjingwei0@foxmail.com>
2024-12-24 20:44:50 +0800

    修改Lc105

--- day16/src/com/wjw/Lc105.java
+++ day16/src/com/wjw/Lc105.java
@@ -1,53 +1,43 @@
 package com.wjw;
 
+import java.util.HashMap;
+
 /*
 从前序和中序中构造二叉树
 均无重复元素
  */
 public class Lc105 {
+    HashMap<Integer, Integer> valToIndex = new HashMap<>();
+
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        // 先找到前序的第一个作为根节点
-        // inorder中找到这个 前面的数组 后面的数组   preorder 中找到对应的 后面的        做递归
-//        if (preorder.length == 1 && inorder.length == 1) {
-//            return new TreeNode(preorder[0]);
-//        }
-//        int n = preorder.length;
-//        int rVal = preorder[0];
-//        // 找到 rVal在inorder的索引
-//        int rValIdx = find(rVal, inorder);
-//        int[] inArr1 = new int[rValIdx];
-//        int[] inArr2 = new int[n - rValIdx - 1];
-//        System.arraycopy(inorder, 0, inArr1, 0, rValIdx);
-//        System.arraycopy(inorder, rValIdx + 1, inArr2, 0, n - rValIdx - 1);
-//
-//        int[] preArr1 = new int[rValIdx];
-//        int[] preArr2 = new int[n - rValIdx - 1];
-//        System.arraycopy(preorder, 1, preArr1, 0, rValIdx);
-//        System.arraycopy(preorder, 1 + rValIdx, preArr2, 0, n - rValIdx - 1);
-//
-//        TreeNode leftTree = buildTree(preArr1,inArr1);
-//        TreeNode rightTree = buildTree(preArr2,inArr2);
-//        TreeNode root = new TreeNode(rVal);
-//        root.left = leftTree;
-//        root.right = rightTree;
-//        return root;
-
-        return null;
-    }
-
-    private int find(int val, int[] inorder) {
         for (int i = 0; i < inorder.length; i++) {
-            if (val == inorder[i]) return i;
+            valToIndex.put(inorder[i], i);
         }
-        return -1; // 找不到-1 这里是可以找到的
+        return build(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1);
     }
+
+    // build函数定义： 前序preorder 范围是preStart preEnd 后续 inorder 范围 inStart inEnd 构造二叉树，返回二叉树根节点。
+    // 不使用范围，需要重新给数组，比较麻烦，看之前的解法，后面考虑边界也比较麻烦。
+    private TreeNode build(int[] preorder, int preStart, int preEnd, int[] inorder, int inStart, int inEnd) {
+        if (preStart > preEnd) {
+            return null;
+        }
+        int rootVal = preorder[preStart];
+        int index = valToIndex.get(rootVal);
+        int leftSize = index - inStart;
+        TreeNode root = new TreeNode(rootVal);
+        root.left = build(preorder, preStart + 1, preStart + leftSize, inorder, inStart, index - 1);
+        root.right = build(preorder, preStart + leftSize + 1, preEnd, inorder, index + 1, index);
+        return root;
+    }
+
 
     public static void main(String[] args) {
         Lc105 test = new Lc105();
-//        int[] pre = {3, 9, 20, 15, 7};
-//        int[] ino = {9, 3, 15, 20, 7};
-        int[] pre = {1, 2};
-        int[] ino = {2, 1};
+        int[] pre = {3, 9, 20, 15, 7};
+        int[] ino = {9, 3, 15, 20, 7};
+//        int[] pre = {1, 2};
+//        int[] ino = {2, 1};
         TreeNode treeNode = test.buildTree(pre, ino);
         System.out.println(treeNode);
     }
